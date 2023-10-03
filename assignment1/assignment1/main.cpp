e0d0b876c1c205b527c01882be77c1ffc77a6d88
ruskrosca <ruskb0197@gmail.com>
2023-10-03 10:34:09 +0900

    課題1の内容をコミット

---
+++ assignment1/assignment1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "BidirectionalList.h"
+
+using namespace std;
+
+int main()
+{
+	// ファイル読み込み
+	string fileName("Assets/Scores.txt");
+	ifstream scoreFile(fileName);
+
+	// 読み込みで来たか
+	if (!scoreFile)
+	{
+		cout << "ファイルオープンに失敗" << endl;
+		cin.get();
+		return false;
+	}
+
+	BidirectionalList bidirectionalList;
+	string line;
+
+	// 一行取り出す
+	while (getline(scoreFile, line))
+	{
+		// リストに追加
+		bidirectionalList.GetList(line);
+	}
+
+	// リストの表示
+	bidirectionalList.Display();
+
+	return 0;
+}
